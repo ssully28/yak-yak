9cc7017d75a418a4091c904be8d6e10e3aa39b20
Steve Sullivan <ssully28@gmail.com>
2019-04-24 10:55:13 -0700

    fixed profile edit

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask import Flask, render_template, request, flash, redirect, session, g
 from flask_debugtoolbar import DebugToolbarExtension
 from sqlalchemy.exc import IntegrityError
 
-from forms import UserAddForm, LoginForm, MessageForm
+from forms import UserAddForm, LoginForm, MessageForm, UserEditForm
 from models import db, connect_db, User, Message
 
 CURR_USER_KEY = "curr_user"
@@ -212,7 +212,33 @@ def stop_following(follow_id):
 def profile():
     """Update profile for current user."""
 
-    # IMPLEMENT THIS
+    if not g.user:
+        flash("Access unauthorized.", "danger")
+        return redirect("/")
+
+    form = UserEditForm(obj=g.user)
+
+    if form.validate_on_submit():
+
+        form_password = form.password.data
+        user = User.authenticate(g.user.username, form_password)
+        if user:
+            user.username = form.username.data
+            user.email = form.email.data
+            user.image_url = form.image_url.data
+            user.header_image_url = form.header_image_url.data
+            user.bio = form.bio.data
+
+            db.session.commit()
+            flash("Profile updated.", 'success')
+            return redirect(f'/users/{user.id}')
+        else:
+            flash("Invalid credentials.", 'danger')
+            return redirect('/')
+        
+    return render_template("users/edit.html", form=form)
+
+        
 
 
 @app.route('/users/delete', methods=["POST"])
